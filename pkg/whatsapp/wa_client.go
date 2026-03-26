4ddb091f3c9a621f2bf5279640443f67713dbce4
Hadyu Fiqran Hakim <hadyuhakim@gmail.com>
2026-03-26 08:57:22 +0700

    refactor: reorganize imports and enhance QR code handling in WhatsApp client initialization

--- pkg/whatsapp/wa_client.go
+++ pkg/whatsapp/wa_client.go
@@ -2,10 +2,13 @@ package whatsapp
 
 import (
 	"context"
+	"fmt"
+	"os"
 
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/mdp/qrterminal/v3"
 	"go.mau.fi/whatsmeow"
 	"go.mau.fi/whatsmeow/store/sqlstore"
-	_ "github.com/mattn/go-sqlite3"
 )
 
 type WhatsappSender interface {
@@ -28,6 +31,22 @@ func NewWaClient() (*waClient, error) {
 	}
 
 	clientWa := whatsmeow.NewClient(deviceStore, nil)
+	if clientWa.Store.ID == nil {
+		qrChan, _ := clientWa.GetQRChannel(context.Background())
+		if err := clientWa.Connect(); err != nil {
+			return nil, err
+		}
+
+		for event := range qrChan {
+			if event.Event == "code" {
+				qrterminal.GenerateHalfBlock(event.Code, qrterminal.L, os.Stdout)
+				fmt.Println("scan for connect to whatsapp")
+			}
+		}
+	} else {
+		clientWa.Connect()
+	}
+	
 	return &waClient{client: clientWa}, nil
 }
 
